2c6663e3c1d911cf1a601fe4e43b0c4069ce90c0
Ahmet KURT <ahmetkurtk2@gmail.com>
2025-02-08 17:27:37 +0300

    a

--- server.js
+++ server.js
@@ -5,11 +5,39 @@ const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 const multer = require("multer");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = 5000;
 
 const JWT_SECRET = "abcdefghjklmnprs";
+const uri =
+  "mongodb+srv://tubitak-admin:uFt7yIN9j8zaE4su@cluster0.x2r3tsi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 // Middleware
 app.use(express.json());
