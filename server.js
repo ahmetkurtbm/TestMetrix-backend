ebe26caed0a0700e21d96371448a29b91a5f3a83
Ahmet KURT <ahmetkurtk2@gmail.com>
2025-02-10 04:38:36 +0300

    cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const uri =
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 
 mongoose
   .connect(uri)
