ae1494ec5dbb4b7cf80bbf2269410a54a383eeb0
Ahmet KURT <ahmetkurtk2@gmail.com>
2025-10-27 00:43:36 +0300

    a

--- server.js
+++ server.js
@@ -25,15 +25,20 @@ if (!JWT_SECRET || !uri || !frontendURL) {
 }
 
 // Middleware
-app.use(express.json({ limit: "16mb" })); // limiti artır
-app.use(express.urlencoded({ extended: true, limit: "16mb" }));
-app.use(cookieParser());
 app.use(
   cors({
     origin: frontendURL,
     credentials: true,
   })
 );
+app.use(express.json({ limit: "16mb" })); // limiti artır
+app.use(express.urlencoded({ extended: true, limit: "16mb" }));
+app.use(cookieParser());
+app.options("*", cors({
+  origin: frontendURL,
+  credentials: true,
+}));
+
 
 // const allowedOrigins = [
 //   "https://testmetrix.com.tr",
