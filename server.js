761f450f72a4418f6badcc6e37eb7ddfc9f82494
Ahmet KURT <ahmetkurtk2@gmail.com>
2025-11-06 23:22:19 +0300

    g

--- server.js
+++ server.js
@@ -25,35 +25,35 @@ if (!JWT_SECRET || !uri || !frontendURL) {
 }
 
 // Middleware
-app.use(
-  cors({
-    origin: frontendURL,
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: frontendURL,
+//     credentials: true,
+//   })
+// );
 app.use(express.json({ limit: "16mb" })); // limiti artır
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
 app.use(cookieParser());
 
-// const allowedOrigins = [
-//   "https://testmetrix.vercel.app",
-//   "http://localhost:3000"
-// ];
+const allowedOrigins = [
+  "https://testmetrix.vercel.app",
+  "http://localhost:3000"
+];
 
-// app.use(
-//   cors({
-//     origin: function (origin, callback) {
-//       if (!origin || allowedOrigins.includes(origin)) {
-//         callback(null, true);
-//       } else {
-//         callback(new Error("Not allowed by CORS"));
-//       }
-//     },
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 mongoose
   .connect(uri)
