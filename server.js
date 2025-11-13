443c9148b8e1ea5e53c060a2c8d045243f56afdb
Ahmet KURT <ahmetkurtk2@gmail.com>
2025-11-13 23:24:51 +0300

    cors

--- server.js
+++ server.js
@@ -34,10 +34,16 @@ if (!JWT_SECRET || !uri || !frontendURL) {
 // );
 // app.options("*", cors()); 
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+//
+
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+//   credentials: true
+// }));
+
+//
+
+app.use(cors());
 
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: true, limit: "16mb" }));
